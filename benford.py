81b8fad5389ea470d36d0cb99edd4bbf410b4e7d
Rafael Mata <rafaelmata357@gmail.com>
2020-11-19 16:10:16 -0600

    Add the initial script file with Benford Class

---
+++ benford.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# PROGRAMMER:    Rafael Mata M.
+# DATE CREATED:  19 Nov 2020                                
+# REVISED DATE:  19 Nov 2020
+# PURPOSE: A package to analyze a dataset of numbers and apply the Benfords Law
+#
+#   Usage:
+#  
+#   
+##
+
+# Imports python modules
+
+class Benford:
+    pass
+
