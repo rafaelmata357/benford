f61b40fdab7539c9f0b030025c1dd7d185bc1e5e
Rafael Mata <rafaelmata357@gmail.com>
2020-11-26 21:28:48 -0600

    setup.py updated with project description

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(name='benford',
-      version='0.1',
-      description='Benford´s Law',
-      packages=['benford'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="benford", # Replace with your own username
+    version="0.1.1",
+    author="Rafael Mata",
+    author_email="rafaelmata357@gmail.com",
+    description="A python tool to calculate the Benford´s Law for a dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rafaelmata357/benford",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    setup_requires=['wheel']
+)
